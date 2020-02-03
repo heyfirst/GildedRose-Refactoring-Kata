01d84bcd3c447c8a3d3234dc5ef808b4f224476a
Kanisorn Sutham <firstziiz.k@gmail.com>
2020-02-03 21:25:41 +0700

    Remove all Sulfuras condition

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -31,8 +31,7 @@ class GildedRose(object):
             else:
                 self.decrease_quality(item)
 
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            item.sell_in = item.sell_in - 1
 
             if item.sell_in < 0:
                 if item.name == "Aged Brie":
