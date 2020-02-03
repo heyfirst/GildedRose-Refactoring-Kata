92aa070f9c9396bb6ea324711a2d0c7fc2229165
Kanisorn Sutham <firstziiz.k@gmail.com>
2020-02-03 21:17:01 +0700

    Convert condition back

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -36,15 +36,11 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
 
-            if item.sell_in >= 0:
-                continue
-
+            if item.sell_in < 0:
             if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            else:
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = item.quality - item.quality
+                    self.increase_quality(item)
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
                 else:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         self.decrease_quality(item)
