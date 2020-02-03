d4ac474713e48025fc3c918bfe15781a4d1bb62f
Kanisorn Sutham <firstziiz.k@gmail.com>
2020-02-03 21:04:10 +0700

    Rotate condition

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -22,8 +22,8 @@ class GildedRose(object):
                         if item.sell_in < 6 and item.quality < 50:
                             item.quality = item.quality + 1
             else:
+                if item.name != "Sulfuras, Hand of Ragnaros":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
                     item.quality = item.quality - 1
 
             if item.name != "Sulfuras, Hand of Ragnaros":
@@ -39,7 +39,8 @@ class GildedRose(object):
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = item.quality - item.quality
                 else:
-                    if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        if item.quality > 0:
                             item.quality = item.quality - 1
 
 
