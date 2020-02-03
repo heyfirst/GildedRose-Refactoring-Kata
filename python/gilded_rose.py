d2ffab99b7b4b672e4c4b9d6c62085997e72d164
Kanisorn Sutham <firstziiz.k@gmail.com>
2020-02-03 20:56:05 +0700

    extract condition

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -17,13 +17,13 @@ class GildedRose(object):
                     item.quality = item.quality + 1
 
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality < 50:
-                            if item.sell_in < 11:
+                        if item.sell_in < 11 and item.quality < 50:
                             item.quality = item.quality + 1
-                            if item.sell_in < 6:
+                        if item.sell_in < 6 and item.quality < 50:
                             item.quality = item.quality + 1
             else:
-                if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
                     item.quality = item.quality - 1
 
             if item.name != "Sulfuras, Hand of Ragnaros":
