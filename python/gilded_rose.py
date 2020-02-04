e6722f0473c2831d7e7ec75a5dfec37012d6d505
Kanisorn Sutham <firstziiz.k@gmail.com>
2020-02-04 19:40:49 +0700

    Refactor to Factory pattern

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,8 +6,27 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
+        for i, item in enumerate(self.items):
+            item = ItemFactory.create_item(item)
             item.update()
+            self.items[i] = item
+
+
+class ItemFactory:
+    @staticmethod
+    def create_item(item):
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            return Sulfuras(name=item.name, sell_in=item.sell_in, quality=item.quality)
+
+        if item.name == "Aged Brie":
+            return AgedBrie(name=item.name, sell_in=item.sell_in, quality=item.quality)
+
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePass(
+                name=item.name, sell_in=item.sell_in, quality=item.quality
+            )
+
+        return Item(name=item.name, sell_in=item.sell_in, quality=item.quality)
 
 
 class Item:
@@ -15,15 +34,6 @@ class Item:
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
-
-        if self.name == "Sulfuras, Hand of Ragnaros":
-            self.__update_func = update_sulfuras
-        elif self.name == "Aged Brie":
-            self.__update_func = update_aged_brie
-        elif self.name == "Backstage passes to a TAFKAL80ETC concert":
-            self.__update_func = update_backstage
-        else:
-            self.__update_func = update_item
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
@@ -37,43 +47,48 @@ class Item:
             self.quality = self.quality + 1
 
     def update(self):
-        self.__update_func(self)
+        self.decrease_quality()
+
+        self.sell_in = self.sell_in - 1
+
+        if self.sell_in < 0:
+            self.decrease_quality()
 
 
-def update_sulfuras(item):
-    return item
+class Sulfuras(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        pass
 
 
-def update_aged_brie(item):
-    item.increase_quality()
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0:
-        item.increase_quality()
+class AgedBrie(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
-    return item
+    def update(self):
+        self.increase_quality()
 
-
-def update_backstage(item):
-    item.increase_quality()
-
-    if item.sell_in < 11:
-        item.increase_quality()
-    if item.sell_in < 6:
-        item.increase_quality()
-
-    item.sell_in = item.sell_in - 1
-
-    if item.sell_in < 0:
-        item.quality = 0
-
-    return item
+        self.sell_in = self.sell_in - 1
+        
+        if self.sell_in < 0:
+            self.increase_quality()
 
 
-def update_item(item):
-    item.decrease_quality()
-    item.sell_in = item.sell_in - 1
+class BackstagePass(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
-    if item.sell_in < 0:
-        item.decrease_quality()
+    def update(self):
+        self.increase_quality()
 
-    return item
+        if self.sell_in < 11:
+            self.increase_quality()
+        if self.sell_in < 6:
+            self.increase_quality()
+
+        self.sell_in = self.sell_in - 1
+
+        if self.sell_in < 0:
+            self.quality = 0
